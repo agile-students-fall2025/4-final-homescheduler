0301a6b62af646f6a056f64f1b8f9e7a206a7788
aaugust22 <aca9900@nyu.edu>
2025-10-23 16:34:19 -0400

    login and homescreen

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import './MyCalender.css'
 import { MyCalendar } from './MyCalender';
+import { Login } from './Login';
+import { HomePage } from './Homepage';
+
+
 function App() {
   return (
    <div>
-      <MyCalendar></MyCalendar>
+      <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/myschedule" element={<MyCalendar />} />
+      </Routes>
+    </Router>
+      
     </div>
   );
 }
