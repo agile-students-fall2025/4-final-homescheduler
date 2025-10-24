f375b2f63a614238873914c19ed209a8a565eaa5
BRANDON RODRIGUEZ <br2412@nyu.edu>
2025-10-23 20:54:47 -0400

    Added Settings > Manage Account route

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,16 +5,20 @@ import './MyCalender.css'
 import { MyCalendar } from './MyCalender';
 import { Login } from './Login';
 import { HomePage } from './Homepage';
+import { Settings } from './Settings';
+import { Manage_Acc } from './Manage_Acc';
 
 
 function App() {
   return (
    <div>
-      <Router>
+    <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/myschedule" element={<MyCalendar />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/manage-account" element={<Manage_Acc />} />
       </Routes>
     </Router>
       
