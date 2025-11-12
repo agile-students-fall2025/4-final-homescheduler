735126122a9255c6f5d1f8a06822fbf37c147bc8
EscoAl516 <escobaralexander2004@gmail.com>
2025-11-11 23:47:26 -0500

    Mergeable

--- back-end/app.js
+++ back-end/app.js
@@ -1,22 +1,27 @@
-// import and instantiate express
-const express = require("express") // CommonJS import style!
+const express = require('express');
 const cors = require('cors'); // We'll need this to let React talk to the server
 
-const remindersRouter = require('./routes/reminders'); // <-- add this
+//import routers
+const userRoutes = require('./controllers/userControl');
+const eventRoutes = require('./controllers/calender');
+const reminderRoutes = require('./controllers/reminders');
 
 
-const app = express() // instantiate an Express object
+const app = express();
 const PORT = 3001; 
+
 
 app.use(cors());
 
-
 app.use(express.json());
 
+// Routers
+app.use('/api/users', userRoutes);
+app.use('/api/events', eventRoutes);
+app.use('/api/reminders', reminderRoutes);
 
-app.use("/api/reminders", remindersRouter);
-module.exports = app;
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
