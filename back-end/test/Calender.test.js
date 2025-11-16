9904bd99fc9c0f53edfeb6ea1e7eb08e9989915b
catherine-g-13 <catherinego051@gmail.com>
2025-11-16 15:55:39 -0500

    calendar test

--- back-end/test/Calender.test.js
+++ back-end/test/Calender.test.js
@@ -81,6 +81,83 @@ describe('Event API (/api/events)', () => {
       expect(res.body.error).to.equal('Missing required fields: title, start, user');
     });
   });
+//catherine
+ // update event test case
+ describe('PUT /api/events/:id', () => {
+  it('should update a preexisting event', async () => {
+    const resGet = await request.get('/api/events');
+    expect(resGet.status).to.equal(200)
+
+    const gymEvent = resGet.body.find(e => e.title === 'Gym');
+    expect(gymEvent).to.exist;
+    
+    
+    const eventIdToUpdate = gymEvent.id
+
+    const updates = {
+      title: 'CANCELLED - Gym',
+      start: '2025-11-14T19:00:00'
+    };
+
+    const res = await request
+      .put(`/api/events/${eventIdToUpdate}`)
+      .send(updates);
+
+    
+    expect(res.status).to.equal(200);
+    expect(res.body).to.be.an('object');
+    expect(res.body.title).to.equal('CANCELLED - Gym');
+    expect(res.body.start).to.equal('2025-11-14T19:00:00');
+    expect(res.body.id).to.equal(eventIdToUpdate);
+
+
+    // Check if the file was  updated
+    const updatedEvents = JSON.parse(fs.readFileSync(liveDataPath, 'utf-8'));
+    const changedEvent = updatedEvents.find(e => e.id === eventIdToUpdate);
+    expect(changedEvent.title).to.equal('CANCELLED - Gym');
+  });
+
+  it('should return 404 if ID is nonexistent', async () => {
+    const res = await request
+      .put(`/api/events/fake-id-123`)
+      .send({ title: 'New Title'});
+
+    expect(res.status).to.equal(404);
+    expect(res.body.error).to.equal('Event not found');
+  });
+});
+
+ // delete event test case
+ describe('DELETE /api/events/:id', () => {
+  it('should delete a preexisting event', async () => {
+    const resGet = await request.get('/api/events');
+    expect(resGet.status).to.equal(200)
+
+    const movingEvent = resGet.body.find(e => e.title === 'Moving Day');
+    expect(movingEvent).to.exist;
+    
+    const eventIdToDelete = movingEvent.id
+
+    const res = await request
+      .delete(`/api/events/${eventIdToDelete}`)
+
+    
+    expect(res.status).to.equal(200);
+    expect(res.body.message).to.equal('Event deleted successfully');
+
+
+    // Check if the file was  updated
+    const updatedEvents = JSON.parse(fs.readFileSync(liveDataPath, 'utf-8'));
+    expect(updatedEvents).to.have.lengthOf(1);
+    expect(updatedEvents[0].title).to.equal('Gym');
+  });
+
+  it('should return 404 if ID is nonexistent', async () => {
+    const res = await request.delete(`/api/events/fake-id-123`);
+    expect(res.status).to.equal(404);
+    expect(res.body.error).to.equal('Event not found');
+  });
+});
 
   // ---end 
 });
