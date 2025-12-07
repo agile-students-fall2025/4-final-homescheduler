c8ad8dd1acc40a4559bb9a3b1d32b065191b6d69
aaugust22 <aca9900@nyu.edu>
2025-12-06 19:50:19 -0500

    homepage updates

--- front-end/src/Homepage.js
+++ front-end/src/Homepage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './HomePage.css'; 
 import logo from './logo.png';
 import { useNavigate } from 'react-router-dom';
@@ -10,6 +10,32 @@ export function HomePage() {
   const user = storedUser && storedUser !== "null" ? JSON.parse(storedUser) : null;
   const username = user?.name || "Guest";
 
+  const [currentDateTime, setCurrentDateTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentDateTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const weekday = currentDateTime.toLocaleString("en-US", {
+    weekday: "long",
+  });
+
+  const date = currentDateTime.toLocaleString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+
+  const time = currentDateTime.toLocaleString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  });
+
   return (
     <div className="home-container">
       <img src={logo} alt="App Logo" className="app-logo" />
@@ -17,8 +43,16 @@ export function HomePage() {
 
       <div className="welcome-box">
         <p>Welcome {username}!</p>
-      </div>
 
+       
+
+      </div>
+      <div className="date-time-widget">
+        <div className="dt-weekday">{weekday}</div>
+        <div className="dt-date">{date}</div>
+        <div className="dt-time">{time}</div>
+      </div>
+      
       <div className="button-group">
         <button className="home-button" onClick={() => navigate('/myschedule')}>
           Master Schedule
