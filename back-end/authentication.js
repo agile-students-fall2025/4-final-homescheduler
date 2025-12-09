1c5a484a1d5fd3c4f5e150ce1ea6097f3ae81cb3
bbranduhn <br2412@nyu.edu>
2025-12-08 23:51:51 -0500

    Create code to join family and view family/user events

--- back-end/authentication.js
+++ back-end/authentication.js
@@ -14,7 +14,7 @@ const auth = (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, "dev_secret_key"); 
+    const decoded = jwt.verify(token, process.env.JWT_SECRET); 
     req.user = decoded;
     next();
   } catch (err) {
