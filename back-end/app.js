f4a77912139433e6f0bb257a6dc70b29b3d84a2a
bbranduhn <br2412@nyu.edu>
2025-12-10 01:09:41 -0500

    route tes

--- back-end/app.js
+++ back-end/app.js
@@ -43,9 +43,9 @@ app.use('/api/calendar', CalRoutes);
 
 app.use(express.static(path.join(__dirname, '../front-end/build')));
 
-app.get('*', (req, res) => {
+/* app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../front-end/build/index.html'));
-});
+}); */
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
