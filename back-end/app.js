054eba9136ad05fc525cbe7986107ccb2e252a79
bbranduhn <br2412@nyu.edu>
2025-12-10 00:32:44 -0500

    revert route

--- back-end/app.js
+++ back-end/app.js
@@ -43,7 +43,7 @@ app.use('/api/calendar', CalRoutes);
 
 app.use(express.static(path.join(__dirname, '../front-end/build')));
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../front-end/build/index.html'));
 });
 
